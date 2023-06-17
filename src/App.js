fe16dddc2dd3ae6bcfe1d03643c5ca8480e64dee
Bipin <bipinyadav9769@gmail.com>
2023-06-17 16:44:42 +0530

    Natours Project setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
             <span className="heading-primary--sub">is where life happens</span>
           </h1>
 
-          <a href="#section-tours" className="btn btn--white btn--animated">
+          <a href="/
+          " className="btn btn--white btn-animated">
             Discover our tours
           </a>
         </div>
